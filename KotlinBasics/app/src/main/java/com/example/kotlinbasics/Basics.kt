c6f48d82c20453d24925aa78b208210bf9be5627
Leraedon <thor0249@gmail.com>
2024-03-16 16:03:34 +0900

    lecture day 3

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,24 +1,48 @@
 package com.example.kotlinbasics
 
+data class CoffeeDetails(
+    val sugarCount: Int,
+    var name: String,
+    var size: String,
+    val creamAmount: Int
+)
 fun main(){
-    val myChar = '\u00AE'
+    var daisy = Dog("Daisy", "Dwarf poodle", 1)
 
-    println(myChar)
+    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age} years old")
 
-    var name = "Denis"
-    name = "FrankBURGER"
-    println(name.lowercase())
+    var myBook = Book()
+    var customBook = Book("Mola", "Someone", "2023")
+    println("${myBook.title}, ${myBook.author}, ${myBook.yearPublished}")
+    println("${customBook.title}, ${customBook.author}, ${customBook.yearPublished}")
 
-    var firstName = "Denis"
-    var lastName = "Panjuta"
-    println(firstName + " " + lastName)
+    val coffeeForDenis = CoffeeDetails(0, "denis", "xxl", 0)
 
-    println("Please enter your age as a whole number")
-    val enteredValue = readln()
-    val age = enteredValue.toInt()
-    if(age >= 18) {
-        println("T")
+    makeCoffee(coffeeForDenis)
+}
+
+fun add(num1: Int, num2: Int): Int {
+    val result = num1 + num2
+    return result
+}
+
+fun askCoffeeDetails() {
+    println("Who is this coffee for?")
+    val name = readln()
+
+    println("How many pieces of sugar do you want?")
+    val sugarCount = readln()
+    val sugarCountInt = sugarCount.toInt()
+
+    //makeCoffee(sugarCountInt, name)
+}
+
+fun makeCoffee(coffeeDetails: CoffeeDetails) {
+    if(coffeeDetails.sugarCount == 1) {
+        println("Coffee with 1 spoon of sugar for ${coffeeDetails.name}")
+    } else if(coffeeDetails.sugarCount == 0) {
+        println("Coffee with no sugar for ${coffeeDetails.name}")
     } else {
-        println("F")
+        println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name}")
     }
 }
---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/BookClass.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinbasics
+
+class Book(val title: String = "Unknown",
+           val author: String = "Anonymous",
+           val yearPublished: String = "2024") {
+
+}
